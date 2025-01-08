88a8541f0a7af36d78a49635f4fe1312be51c403
Fabio Molon <fabio.molon@athonet.com>
2025-01-08 10:29:22 +0100

    Rewrite name register for aht10 sensor

--- components/aht10/aht10.c
+++ components/aht10/aht10.c
@@ -5,17 +5,14 @@
 #include "driver/i2c.h"
 #include "aht10.h"
 
-#define AHT10_ADDRESS          0x38
-#define INITIALIZATION_COMMAND 0XE1
-#define TRIGGER_MEASUREMENT    0xAC
-#define INIT0_AHT10            0x08
-#define INIT1_AHT10            0x00
-#define DATA_0                 0x33
-#define DATA_1                 0x00
-#define AHT10_SOFT_RESET       0xBA
-#define AHT10_BUSY             0x80
-#define AHT10_MODE             0x60
-#define AHT10_CALIBRATION      0x04
+#define AHT10_ADDRESS                0x38
+#define AHT10_INITIALIZATION_COMMAND 0XE1
+#define AHT10_TRIGGER_MEASUREMENT    0xAC
+#define AHT10_INIT0                  0x08
+#define AHT10_INIT1                  0x00
+#define AHT10_DATA_0                 0x33
+#define AHT10_DATA_1                 0x00
+#define AHT10_SOFT_RESET             0xBA
 
 #define TIMEOUT 1000
 
@@ -67,9 +64,9 @@ esp_err_t trigger_measurement_aht10() {
 
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd, AHT10_ADDRESS << 1 | I2C_MASTER_WRITE, I2C_MASTER_ACK);
-	i2c_master_write_byte(cmd, TRIGGER_MEASUREMENT, I2C_MASTER_ACK);
-	i2c_master_write_byte(cmd, DATA_0, I2C_MASTER_ACK);
-	i2c_master_write_byte(cmd, DATA_1, I2C_MASTER_ACK);
+	i2c_master_write_byte(cmd, AHT10_TRIGGER_MEASUREMENT, I2C_MASTER_ACK);
+	i2c_master_write_byte(cmd, AHT10_DATA_0, I2C_MASTER_ACK);
+	i2c_master_write_byte(cmd, AHT10_DATA_1, I2C_MASTER_ACK);
     i2c_master_stop(cmd);
 
     err = i2c_master_cmd_begin(I2C_NUM_0, cmd, TIMEOUT / portTICK_RATE_MS);
@@ -98,9 +95,9 @@ esp_err_t init_aht10() {
 
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd, AHT10_ADDRESS << 1 | I2C_MASTER_WRITE, I2C_MASTER_ACK);
-	i2c_master_write_byte(cmd, INITIALIZATION_COMMAND, I2C_MASTER_ACK);
-	i2c_master_write_byte(cmd, INIT0_AHT10, I2C_MASTER_ACK);
-	i2c_master_write_byte(cmd, INIT1_AHT10, I2C_MASTER_ACK);
+	i2c_master_write_byte(cmd, AHT10_INITIALIZATION_COMMAND, I2C_MASTER_ACK); // 0xE1
+	i2c_master_write_byte(cmd, AHT10_INIT0, I2C_MASTER_ACK); // 0x08
+	i2c_master_write_byte(cmd, AHT10_INIT1, I2C_MASTER_ACK); // 0x00
     i2c_master_stop(cmd);
 
     err = i2c_master_cmd_begin(I2C_NUM_0, cmd, TIMEOUT / portTICK_RATE_MS);
@@ -129,7 +126,7 @@ esp_err_t read_measurement_aht10(uint8_t *data, uint8_t size_data, uint8_t *stat
 
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd, AHT10_ADDRESS << 1 | I2C_MASTER_READ, I2C_MASTER_ACK);
-	i2c_master_read(cmd, data, size_data, I2C_MASTER_ACK);
+	i2c_master_read(cmd, data, size_data, I2C_MASTER_LAST_NACK);
     i2c_master_stop(cmd);
 
     err = i2c_master_cmd_begin(I2C_NUM_0, cmd, TIMEOUT / portTICK_RATE_MS);
@@ -148,7 +145,7 @@ esp_err_t read_measurement_aht10(uint8_t *data, uint8_t size_data, uint8_t *stat
 /* Get temperature value */
 float get_temperature_aht10(uint8_t *data) {
 
-	uint32_t raw_temperature = (((uint32_t)data[3] & 0x0F) << 16) | ((uint16_t)data[4] << 8) | data[5];
+    uint32_t raw_temperature = ((data[3] & 0x0F) << 16) | (data[4] << 8) | data[5];
 
     return ((float)raw_temperature * 200.0 / 1048576.0) - 50.0;
 }
@@ -156,7 +153,7 @@ float get_temperature_aht10(uint8_t *data) {
 /* Get humidity value */
 float get_humidity_aht10(uint8_t *data) {
 
-	int32_t raw_humidity = ((uint32_t)data[1] << 12) | ((uint32_t)data[2] << 4) | (data[3] >> 4);
+    uint32_t raw_humidity = ((data[1] << 16) | (data[2] << 8) | data[3]) >> 4;
 
     return (float)raw_humidity * 100.0 / 1048576.0;
 }
