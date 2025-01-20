bcf557aa861865ec76579b1e92d4129a610ae13d
Fabio Molon <fabio.molon@athonet.com>
2025-01-20 10:37:59 +0100

    Remove old library

--- components/enp/include/enp.h
+++
@@ -1,8 +0,0 @@
-#ifndef ENP_H
-#define ENP_H
-
-esp_err_t conf_esp_now_protocol(esp_now_peer_info_t *peer);
-
-esp_err_t send_espnow_protocol(esp_now_peer_info_t *peer, float temperature, float humidity);
-
-#endif
